076c5c9c396fb275b768b853f3684649b267a5d0
Tazim <tazimulsohag@gmail.com>
2024-01-01 16:39:53 +0600

    txt view added

--- app/src/main/java/co/banglabs/weatherapp/MainActivity.java
+++ app/src/main/java/co/banglabs/weatherapp/MainActivity.java
@@ -3,12 +3,17 @@ package co.banglabs.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView= findViewById(R.id.intro);
+
+
     }
 }
