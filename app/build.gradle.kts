8d8e0c49162f96cdbbcbc05c8d4799cb99184dfa
Tazim <tazimulsohag@gmail.com>
2024-01-06 21:08:45 +0600

    toolbar design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.airbnb.android:lottie:3.4.1")
+    implementation ("com.github.bkhezry:MaterialSearchView:1.6.0")
 }
