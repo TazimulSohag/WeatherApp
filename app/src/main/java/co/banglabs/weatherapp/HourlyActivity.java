de06ab99228b45dcd08eb38d024fd6520fe78cf2
Tazim <tazimulsohag@gmail.com>
2024-01-09 17:37:28 +0600

    activity hourly base design

--- app/src/main/java/co/banglabs/weatherapp/HourlyActivity.java
+++ app/src/main/java/co/banglabs/weatherapp/HourlyActivity.java
@@ -2,9 +2,13 @@ package co.banglabs.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 public class HourlyActivity extends AppCompatActivity {
+
+    private Typeface typeface;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
